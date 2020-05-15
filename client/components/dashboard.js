c7047eaf5d8510ae52073e17605fd7a38244bac9
oleksii <oleksiizhukovskyi@gmail.com>
2020-05-15 19:10:41 +0300

    fixed errors

--- client/components/dashboard.js
+++ client/components/dashboard.js
@@ -1,22 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import Head from './head'
 
-const dashboard = () => {
+const Dashboard = () => {
   return (
     <div>
-      <Head title="Hello" />
-      <div className="flex items-center justify-center h-screen">
-        <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
-          <div id="title">dashboard </div>
-          <Link to="/dashboard/profile/470ef5cc-1325-4d29-b775-498df8cd163f"> Go To Profile  </Link>
-          <Link to="/dashboard/main">  Go To Main</Link>
-        </div>
-      </div>
+      <div id="title">Dashboard </div>
+      <Link to="/dashboard/profile/470ef5cc-1325-4d29-b775-498df8cd163f"> Go To Profile  </Link>
+      <Link to="/dashboard/main">  Go To Main</Link>
     </div>
   )
 }
 
-dashboard.propTypes = {}
+Dashboard.propTypes = {}
 
-export default dashboard
+export default Dashboard
