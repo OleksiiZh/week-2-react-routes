3a6b6fb73bc24c6b07e8559cf36f9cba37c695dc
oleksii <oleksiizhukovskyi@gmail.com>
2020-05-15 19:38:11 +0300

    fixed errors2

--- client/components/main.js
+++ client/components/main.js
@@ -5,7 +5,7 @@ const Main = () => {
   return (
     <div>
       <div id="title"> Main </div> 
-      <Link to="/dashboard/profile/470ef5cc-1325-4d29-b775-498df8cd163">Go To Profile</Link>
+      <Link to="/dashboard/profile/470ef5cc-1325-4d29-b775-498df8cd163f">Go To Profile</Link>
       <Link to="/dashboard">      Go To Root</Link>
     </div>
   )
